6fb8cf31a3b8e38aeff403f23207cc762a88bc58
Altuğ <75982391+AltugGLN@users.noreply.github.com>
2021-06-28 18:39:09 +0300

    Change no of product cards per media query

--- js/main.js
+++ js/main.js
@@ -7,4 +7,38 @@ new Glider(document.querySelector(".glider"), {
         prev: ".glider-prev",
         next: ".glider-next",
     },
+    
+    responsive: [
+        {
+          // screens greater than >= 600px
+          breakpoint: 600,
+          settings: {
+            // Set to `auto` and provide item width to adjust to viewport
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            // itemWidth: 170,
+            duration: 0.25,
+          }
+        },{
+            // screens greater than >= 900px
+            breakpoint: 900,
+            settings: {
+              // Set to `auto` and provide item width to adjust to viewport
+              slidesToShow: 3,
+              slidesToScroll: 3,
+            //   itemWidth: 170,
+              duration: 0.25,
+            }
+          },{
+          // screens greater than >= 1200px
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            // itemWidth: 170,
+            duration: 0.25,
+          }
+        }
+    ]
 });
+
